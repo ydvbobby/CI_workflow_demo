549f4658ad58d69bc94698bd3d600982307dd3a7
Bobby Kumar <bobbyyadav1950@gmail.com>
2026-02-13 13:28:49 +0530

    fix final linting issues

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-print('hello')
+print ('hello')
 
 
 def sum (a, b):
     return a+b
 
 
-print(sum(4,5))
+print (sum(4, 5))
 
 def mul (a, b):
     return a*b
 
 
-print(mul(4, 5))
+print (mul(4, 5))
